d35dea836fd2b57619c303bb54c135ee98ab6b73
Oliver Foster <oliver.foster@kineo.com>
2017-02-21 11:55:44 +0000

    Updated readme

--- README.md
+++ README.md
@@ -1,2 +1,74 @@
 # enum
 A basic enumeration library for JavaScript
+
+Simple Usage:
+```javascript
+
+var STATES = ENUM([
+	"ON",
+	"OFF",
+	"RED",
+	"BLUE"
+]);
+	
+/*
+
+STATES.ON == 1
+STATES.OFF == 2
+STATES.RED == 4
+STATES.BLUE == 4
+
+STATES.ON.asString == "ON"
+STATES.ON.asInteger == 1
+
+STATES[1] = STATES.ON
+STATES[2] = STATES.OFF
+STATES[3] = STATES.RED
+STATES[4] = STATES.BLUE
+
+STATES(1) == STATES.ON
+STATES(-100) == undefined
+STATES("ON") == STATES.ON
+STATES("on") == undefined
+
+
+var plain = STATES.ON
+(plain == STATES.ON) == TRUE
+(plain == 1) == true
+(plain == STATES.OFF) == false
+
+var switched = (STATES.ON + STATES.OFF + STATES.BLUE)
+(switched & STATES.ON) == 1
+(switched & STATES.RED) == 0
+(switched & STATES.BLUE) == 8
+
+
+*/
+
+```
+
+
+Define a lookup value modifier:
+```javascript
+
+var STATES = ENUM([
+	"ON",
+	"OFF",
+	"RED",
+	"BLUE"
+], function statesLookup(value) {
+	
+	if (typeof value == "string") return value.toUpperCase();
+	return value;
+
+});
+	
+/*
+
+STATES.ON == 1
+STATES("ON") == 1
+STATES("on") == 1
+
+*/
+
+```
--- enum.js
+++ enum.js
@@ -1,41 +1,45 @@
 //2017-02-21 BASIC ENUMERATION SUPPORT
 (function() {
 
-    function ENUM(namesArray, delegatedLookupFunction) {
+    function ENUM(namesArray, lookupModifierFunction) {
 
         if (!(namesArray instanceof Array)) throw "First argument of ENUM must be an array";
 
         // Create lookup & storage function
         var ENUMERATION = function(lookupValue){
-            if (delegatedLookupFunction) lookupValue = delegatedLookupFunction(lookupValue);
+
+            if (lookupModifierFunction) {
+                lookupValue = lookupModifierFunction(lookupValue);
+            }
+           
             return ENUMERATION[lookupValue];
+
         };
 
         for (var i = 0, l = namesArray.length; i < l; i++) {
+
             var names = namesArray[i];
             if (!(names instanceof Array)) names = [names];
 
             // Make each value a power of 2 to allow for bitwise switches
             var value = Math.pow(2, i);
 
-            for (var n = 0, m = names.length; n < m; n++) {            
-                var name = names[n];
+            for (var n = 0, m = names.length; n < m; n++) {
 
-                // Create each named entry
-                var uname = name.toUpperCase();
+                var name = names[n];
 
                 // Create Number object to allow for primitive comparisons and JSON stringify
                 var entry = new Number(value);
 
                 // Assign conversion values to entry
-                entry.asString = uname;
+                entry.asString = name;
                 entry.asInteger = value;
 
                 // Reference lookup & storage function from each entry
                 entry.ENUM = ENUMERATION;
 
                 // Add entry to lookup & storage function
-                ENUMERATION[uname] = ENUMERATION[value] = entry;
+                ENUMERATION[name] = ENUMERATION[value] = entry;
 
             }
 
